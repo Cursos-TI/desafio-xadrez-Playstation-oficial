2318870edc8fddad80416dc2d26387153a281eb5
Playstation-oficial <overigor313@gmail.com>
2025-11-22 21:43:16 +0000

    trabalho

--- xadrez.c
+++ xadrez.c
@@ -2,9 +2,20 @@
 
 int main() {
 
+    int menu;
+
+    printf("Bem vindo ao jogo de xadrez \n"); ///menu de opções
+    printf("1. para jogar\n");
+    printf("2. para sair\n");
+    printf("digite:");
+    scanf("%d", &menu); ///digite
+    printf("\n");
+
     int movimentosBaixo = 2;   // O cavalo anda duas casas para baixo
     int movimentosEsquerda = 1; // ...e uma para a esquerda
-
+switch (menu)
+{
+case 1:
     printf("Movimento do Cavalo em L:\n");
 
     // Primeiro loop: for → desce 2 vezes
@@ -21,6 +32,18 @@ int main() {
         printf("Esquerda\n");
         j++;
     }
- 
+    break;
+
+    case 2: ///desligando
+       printf("fechando... \n");
+       break;
+default:
+/// quando escolhe nenhuma opção
+
+printf("erro \n");
+
+    break;
+}
+    
     return 0;
 }
